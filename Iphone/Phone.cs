93dfeb0d5144622286c7acc55527f4b67636d193
ukiD0 <valentinaaakkk@yandex.ru>
2023-09-17 00:13:12 +0300

    do have a goooood day with us

---
+++ Iphone/Phone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iphone
+{
+    internal class Phone
+    {
+        public string number;
+        public string model;
+        public string weight;
+        public string _callerName;
+        public string _callerNumber;
+
+        public Phone(string number,string model,string weight) 
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+            
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Модель {model}");
+            Console.WriteLine($"Серия/номер {number}");
+            Console.WriteLine($"Вес {weight}");
+        }
+
+        public void ReceiveCall(string _callerName,string _callerNumber) 
+        {
+            Console.WriteLine($"ОТВЕТЬ УЖЕ ЕМУ - {_callerName}\nНомер телефона {_callerNumber}");
+        }
+
+        public string _getNumber()
+        {
+            return _callerNumber ;
+        }
+
+        public void sendMessage(params string[] _nuners) 
+        {
+            Console.WriteLine("По какому номеру отправить сообщение :* ?");
+            foreach (string _nuner in _nuners)
+            {
+                Console.WriteLine(_nuner);
+            }
+
+        }
+        
+    }
+}
---
+++ Iphone/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iphone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone("1232323","Iphone11","150.01");
+            Phone phone2 = new Phone("222323", "Iphone22", "333");
+            Phone phone3 = new Phone("3333", "Iphone932", "69");
+
+            Console.WriteLine("Информация о телефоне 1");
+            phone1.Info();
+            phone1.ReceiveCall("Evgeniiiii Polivanov", "832883");
+            phone1.sendMessage("89772550187");
+            Console.WriteLine();
+
+            Console.WriteLine("Информация о телефоне 2");
+            phone2.Info();
+            phone2.ReceiveCall("Mammmaaa","322232323");
+            phone2.sendMessage("89772550187");
+            Console.WriteLine();
+
+            Console.WriteLine("Информация о телефонеee 3");
+            phone3.Info();
+            phone3.ReceiveCall("mOSHEINIKII","6696955969");
+            phone3.sendMessage("89772550187");
+            Console.WriteLine();
+
+        }
+    }
+}
